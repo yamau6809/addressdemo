743a0ea90fbf0e82efd120791987ac7ef3652485
Hidemoto Yamauchi <yamau6809@gmail.com>
2022-07-31 16:38:10 -0500

    Initial Commmit

---
+++ src/main/java/com/algorithmicblue/addressdemo/AddressdemoApplication.java
@@ -0,0 +1,13 @@
+package com.algorithmicblue.addressdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algorithmicblue/addressdemo/AddressdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algorithmicblue.addressdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
